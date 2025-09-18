b5357c81e380167769abd0b8cb164aee4eb8aaa7
W41k3R <piyushbhati.daman@gmail.com>
2025-09-18 21:30:07 +0530

    Added Calculation Module
    Added a calculation module which calculates the HMPI using the measured value from input data and using the standards already provided within the code (Source: WHO), this module appends a row named "HMPI" at the end of the Dataframe

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ print("Script started")  # Add this line at the top
 import pandas as pd
 
 from backend.features.data_processing import DataProcessor
+from backend.features.hmpi_calculation import  HMPICalculation
 
+# Processing Input Data
 processor = DataProcessor()
-
-df=processor.load('hmpi-calculator\data\datanew.csv')
-
-
+df=processor.load('data/datanew.csv') 
 print("Loaded DataFrame shape:", df.shape)
-print(df.head())
+print(df)
+print("Data Loaded Successfully")
+
+
+# Calculating HMPI
+print("Running Calculation Module")
+HMPICalculation().calculate(df)
 print(df)
 
 print("It Works")
---
+++ backend/features/hmpi_calculation.py
@@ -0,0 +1,50 @@
+#HMPI Calculations
+
+import pandas as pd
+
+class HMPICalculation:
+
+    def __init__(self):
+        # Defining standards
+        self.standards = {
+            'As': {'S': 0.01, 'W': 100,   'I': 0},
+            'Cd': {'S': 0.003, 'W': 333.3, 'I': 0},
+            'Cr': {'S': 0.05, 'W': 20,    'I': 0},
+            'Pb': {'S': 0.01, 'W': 100,   'I': 0},
+            'Hg': {'S': 0.001, 'W': 1000,  'I': 0},
+            'Ni': {'S': 0.02, 'W': 50,    'I': 0},
+            'Cu': {'S': 1.5, 'W': 0.67,   'I': 0},
+            'Zn': {'S': 3, 'W': 0.33,     'I': 0},
+            'Fe': {'S': 0.3, 'W': 3.33,   'I': 0},
+            'Mn': {'S': 0.1, 'W': 10,     'I': 0},
+            'Co': {'S': 0.05, 'W': 20,    'I': 0},
+            'Al': {'S': 0.2, 'W': 5,      'I': 0},
+            'Se': {'S': 0.01, 'W': 100,   'I': 0},
+            'Sb': {'S': 0.005, 'W': 200,   'I': 0},
+            'Ba': {'S': 0.7, 'W': 1.43,   'I': 0},
+            'V': {'S': 0.05, 'W': 20,     'I': 0}
+            }
+
+
+    def calculate(self,df):
+        hmpi_list=[]
+        for index, row in df.iterrows():
+            num=0
+            den=0
+            for metal, vals in self.standards.items():
+                Mi = row.get(metal,0)
+                Si = vals['S']
+                Ii = vals['I']
+                Wi = vals['W']
+                
+                Qi=100*(Mi-Ii)/(Si-Ii)
+                num+= Wi*Qi
+                den+= Wi
+            hmpi=num/den
+            hmpi_list.append(hmpi)
+        
+        df['HMPI']=hmpi_list
+        return df
+
+            
+
