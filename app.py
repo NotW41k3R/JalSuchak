4d629591ebcb2cbfa9dfd647040230eb3edc5cac
W41k3R <piyushbhati.daman@gmail.com>
2025-09-21 22:57:36 +0530

    Added Geospatial Analysis
    Added Geospatial analysis, which shows HMPI with there sample id on the respective coordinate and categorises it based on colour

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from backend.features.data_processing import DataProcessor
 from backend.features.hmpi_calculation import  HMPICalculation
 from backend.features.basic_output import HMPIOutput
 from backend.features.better_df import PrettyColumns
+from backend.features.geospatial_analysis import GeoSpatialAnalyser
 from werkzeug.utils import secure_filename
 
 app = Flask(
@@ -24,6 +25,7 @@ processor = DataProcessor()
 calculator = HMPICalculation()
 outputter = HMPIOutput()
 format = PrettyColumns()
+geospatial = GeoSpatialAnalyser()
 
 
 # Home Page
@@ -49,6 +51,9 @@ def upload_file():
     df=processor.load(filepath) 
     print("Data Loaded Successfully")
 
+    df.to_pickle(os.path.join('data', 'uploads', 'df_cache.pkl'))
+    session['df_cache'] = os.path.join('data', 'uploads', 'df_cache.pkl')
+
     # Saving filepath in session 
     session['uploaded_file'] = filepath
 
@@ -73,6 +78,7 @@ def calculate_hmpi():
     print("Running Calculation Module")
     df = calculator.calculate(df)
     print(df)
+    df.to_pickle(session['df_cache'])
 
     df2 = format.prettify(df)
     print("It Works")
@@ -84,9 +90,21 @@ def calculate_hmpi():
         'preview': df2.to_dict(orient='records')
         })
 
-# # Generate Map
-# @app.route('/map', methods=['GET'])
-# def generate_map():
+# Generate Map
+@app.route('/map', methods=['GET'])
+def generate_map():
+
+    if 'df_cache' not in session:
+        return "No data uploaded", 400
+    
+    df = pd.read_pickle(session['df_cache'])
+
+    print("Running Geospatial Analysis Module")
+    if processor.coordinates_check(df):
+        geospatial.geospatial_analysis(df)
+        return render_template('map.html')
+    else:
+        return "No coordinates found in data.", 400
 
 
 if __name__ == '__main__':
